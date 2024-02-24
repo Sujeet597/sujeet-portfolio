93ccaaf774bac9e69eb8bfc9252fd88035874c51
[Sujeet Kushwaha] <[kushwahasujeet222@gmail.com]>
2024-02-24 17:19:12 +0530

    code commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,24 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
+// ================= ACCORDIAN SKILLS ===================
+
+const skillsContent = document.getElementsByClassName('skills_content');
+const skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+   let itemClass = this.parentNode.className; // Change ClassName to className
+
+   for (let i = 0; i < skillsContent.length; i++) { // Add 'let' before i
+      skillsContent[i].className = 'skills_content skills_close';
+   }
+
+   if (itemClass === 'skills_content skills_close') {
+      this.parentNode.className = 'skills_content skills_open';
+   }
+}
+
+// Convert NodeList to array and then use forEach
+Array.from(skillsHeader).forEach((el) => {
+   el.addEventListener('click', toggleSkills);
+});
